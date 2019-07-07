dac4c65b3d5b170f6eaf88f53b9dafc45e54543c
Billy.Zheng <vil963@gmail.com>
2019-07-08 00:26:33 +0800

    update puma config.

--- config/containers/app/config/puma_production.rb
+++ config/containers/app/config/puma_production.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8; mode:ruby; -*-
 
-fail 'Need specify APP_FULL_PATH' if ENV['APP_FULL_PATH'].nil?
-
 app_path = ENV['APP_FULL_PATH']
+
+fail 'Need specify APP_FULL_PATH' if app_path.nil?
 
 # Following instruction is need for phased restart.
 directory app_path
@@ -15,16 +15,18 @@ state_path "#{pid_dir}/puma.state"
 environment ENV.fetch('RAILS_ENV', 'production')
 threads 0, ENV.fetch('RAILS_MAX_THREADS', 16).to_i
 require 'etc'; workers ENV.fetch('WEB_CONCURRENCY', Etc.nprocessors).to_i
+
 # socket 文件放到 tmp 目录下, 避免 socket 在系统 /tmp 某些 linux 发布版拒绝访问.
 bind "unix://#{app_path}/tmp/puma.sock"
-# port = ENV.fetch('DEFAULT_PORT', 3000)
+if (port=ENV['DEFAULT_PORT'])
+  bind "tcp://0.0.0.0:#{port}"
+end
 
-# if ENV['RAILS_LOG_TO_STDOUT'] == 'true'
+if ENV['RAILS_LOG_TO_STDOUT'] == 'true'
   stdout_redirect "log/puma.access.log", "log/puma.err.log"
-  # daemonize
-# end
+end
 
-# bind "tcp://0.0.0.0:#{port}" unless port.nil?
+daemonize
 
 plugin :tmp_restart
 
