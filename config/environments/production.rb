dc4c126f68614b963cb6fe3b3351bc4b9326f1f1
Billy.Zheng <vil963@gmail.com>
2019-07-09 16:30:50 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_options = lambda do |event|
     {
-      exception: event.payload[:exception], 
+      exception: event.payload[:exception],
       exception_object: event.payload[:exception_object]
     }
   end
