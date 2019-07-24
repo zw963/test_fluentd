aa0d5e7976c119eda10f23f3ca64f3a4dfba566d
Billy.Zheng <vil963@gmail.com>
2019-07-24 21:42:23 +0800

    Refactor

--- config/initializers/subscriber.rb
+++ config/initializers/subscriber.rb
@@ -11,10 +11,39 @@ class HTTPLogger < AppLogger
   end
 end
 
-ActiveSupport::Notifications.subscribe('start_request.http') do |name, start, finish, id, request|
-  HTTPLogger.logger.info(name: 'start_request.http', start: start, finish: finish, cost_time: finish - start, id: id, payload: request)
+ActiveSupport::Notifications.subscribe('start_request.http') do |name, start_time, finish_time, id, request|
+  req = request.dig(:request)
+
+  payload = {headers: req.headers, request_verb: req.verb}
+
+  HTTPLogger.logger.info(
+    name: 'start_request.http',
+    start: start_time,
+    finish: finish_time,
+    cost_time: finish_time - start_time,
+    msg: req.uri.to_s,
+    tags: [id],
+    payload: payload
+  )
 end
 
-ActiveSupport::Notifications.subscribe('request.http') do |name, start, finish, id, response|
-  HTTPLogger.logger.info(name: 'done_request.http', start: start, finish: finish, cost_time: finish - start, id: id, payload: response)
+ActiveSupport::Notifications.subscribe('request.http') do |name, start_time, finish_time, id, response|
+  res = response.dig(:response)
+  code = res.code
+
+  payload = {headers: res.headers, status_code: code, reason: res.reason}
+
+  if code != 200
+    payload.update(body: res.body.to_s)
+  end
+
+  HTTPLogger.logger.info(
+    name: 'done_request.http',
+    start: start_time,
+    finish: finish_time,
+    cost_time: finish_time - start_time,
+    msg: res.uri.to_s,
+    tags: [id],
+    payload: payload
+  )
 end
