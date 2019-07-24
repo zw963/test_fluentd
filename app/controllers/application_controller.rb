4211ba60f556e4cddeb7bcf08b9f16a168499f20
Billy.Zheng <vil963@gmail.com>
2019-07-25 00:56:28 +0800

    Fix bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   def current_user
     User.find_by(name: 'Billy.Zheng')
   end
--- lib/fluent_logger.rb
+++ lib/fluent_logger.rb
@@ -21,7 +21,8 @@ end
 module ActiveSupport::TaggedLogging::Formatter
   def call(severity, time, progname, data)
     data = { msg: data.to_s } unless data.is_a?(Hash)
-    data[:tags] = current_tags
+    data[:tags] ||= []
+    data[:tags].concat current_tags
 
     super(severity, time, progname, data)
   end
